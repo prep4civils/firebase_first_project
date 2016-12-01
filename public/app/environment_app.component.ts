ca5b4e35de5bf75d75a3d35dae01981e8acf0b04
Ram <prep4civils@gmail.com>
2016-12-01 15:26:12 +0530

    added angular

---
+++ public/app/environment_app.component.ts
@@ -0,0 +1,13 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'my-app'
+})
+
+@View({
+  template: '<h2>Angular with Firebase</h2>'
+})
+
+export class AppComponent {
+
+}
